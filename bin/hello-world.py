6bd6b39ac536c180a58f871370a054f68a6ab2e4
SergeyMC9730 <57207009+SergeyMC9730@users.noreply.github.com>
2021-04-08 22:15:49 +0800

    Update hello-world.py

--- bin/hello-world.py
+++ bin/hello-world.py
@@ -1,1 +1,23 @@
+import sys, json, os, importlib
+from colorama import Back, Fore, Style
+from colorama import init as cinit
+import argparse
+sys.path.insert(0, "../types")
+cinit()
+from binary import Binary
+class HelloWorld(Binary):
+    def __init__(self):
+        self.info = {
+            "name" : "Hello World program",
+            "version" : "v1",
+            "codename": "helloworld",
+            "dependencies" : [], # Not Supported.
+            "run": self.run,
+            "on_load": self.on_load
+        }
 
+    def on_load(self, info):
+        pass #do something
+
+    def run(self, info, pyt):
+        print("test")
