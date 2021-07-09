b381474b5896535c5d9b6409c7e492a737d298c4
Wiki Updater <KrutosVIP@users.noreply.github.com>
2021-07-09 18:02:48 +0400

    Update bashlex_wrap.py

--- libs/PyT/bashlex_wrap.py
+++ libs/PyT/bashlex_wrap.py
@@ -1,2 +1,11 @@
 # Bash-lex wrapper.
 import bashlex
+
+i = "sudo rm -rf /* > log.txt && cat log.txt || export a='mom' && echo $(pwd):$a"
+def _parser(text):
+    parse_ = bashlex.parse(text)
+    for i2 in parse_:
+        print(" ")
+        print(i2.dump())
+
+_parser(i)
