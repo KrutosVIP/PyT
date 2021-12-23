f219ae0f31b7a7b14baabb1d1d38cf1043d6f58c
Wiki Updater <krutosviprus@gmail.com>
2021-12-23 21:10:52 +0400

    Kernel Patches
    > Added chardet to requirments
    > kernel is now packed without startup
    > early init implement
    > some patches to kinit.py

--- kernel/kernel.py
+++ kernel/kernel.py
@@ -44,75 +44,6 @@ class BaseKernel:
             "ktype": "PyZen-PyT"
         }
         
-    def early_hooks(self):
-        debug = self.info['debug']
-        if not os.path.isdir("../etc/"):
-            os.mkdir("../etc/")
-        if not os.path.isdir("../etc/systemctl"):
-            os.mkdir("../etc/systemctl")
-        if not os.path.isdir("../etc/iceberg"):
-            os.mkdir("../etc/iceberg")
-
-        self.basefs = os.getcwd()
-        if not os.path.isdir(f"{self.basefs}/../data"):
-            os.mkdir(f"{self.basefs}/../data")
-        if debug:
-            print("DEBUG: Reading language...")
-        self.lang = self.json_load("../var/kernel_sets.json")["lang"]
-        
-        if debug:
-            print("DEBUG: Reading language files...")
-        try:
-            self.lines = self.json_load(f"../lang/global_{self.lang}.json")
-        except FileNotFoundError:
-            print("[WARNING] Language " + self.lang + " doesn't exists! Setting language to en...")
-            self.lines = self.json_load(f"../lang/global_en.json")
-        self.ksets = self.json_load(f"{self.basefs}/../var/kernel_sets.json")
-        self.boot_opt = self.json_load(f"{self.basefs}/../var/boot.json")
-
-        
-        if self.boot_opt["safe_mode"]:
-            if os.path.exists(f"{self.basefs}/../etc/kbackup/safe.lock"):
-                try:
-                    os.remove(f"{self.basefs}/../etc/kbackup/safe.lock")
-                    copyfile(f"{self.basefs}/../etc/kbackup/kernel_sets.json", f"{self.basefs}/../var/kernel_sets.json")
-                    copyfile(f"{self.basefs}/../etc/kbackup/build_prop.json", f"{self.basefs}/../var/build_prop.json")
-                except:
-                    pass
-                self.safe_boot()
-            else:
-                self.safe_boot()
-
-        if debug:
-            print(f"{self.lines['kernel']['kstart_0_debug']} {self.info['name']} {self.info['version']} {self.lines['kernel']['kstart_1']} {self.info['shell']}")
-        else:
-            print(f"{self.lines['kernel']['kstart_0']} {self.info['name']} {self.info['version']} {self.lines['kernel']['kstart_1']} {self.info['shell']}")
-
-        if self.info["custom"]:
-            print(self.lines['kernel']['kernel_mod'])
-
-        if self.ksets["danger"]:
-            print(self.lines['kernel']['kernel_danger'])
-
-        if self.ksets["expm"]:
-            print(self.lines['kernel']['kernel_expm'])
-          
-        if debug:
-            print(f"{self.lines['kernel']['interpretator']} {sys.version}")
-        if debug:
-            print(self.lines['kernel']['os_mem_fs_start'])
-        
-        if debug:
-            print(f"DEBUG: {self.lines['debug']['osfs_c']}")
-        self.mount_all()
-        if debug:
-            print(self.lines['kernel']['load_mod_bin'])
-            
-        self.modules = load_modules(self.info["module"], self.lines['modules'], debug)
-        if debug:
-            print(self.lines['kernel']['startup'])
-        
-        
 
     def login(self, user, passwd):
         with open(f"{self.basefs}/../var/kernel_sets.json", "r") as f:
@@ -174,46 +105,7 @@ class BaseKernel:
         except KeyboardInterrupt:
             pass
     
-    def startup(self):
-        self.early_hooks()
-        debug = self.info['debug']
-        self.user = None
-        e = 0
-        for d in self.info["dependencies"]:
-            if not d in self.modules:
-                print(self.lines['kernel']["startup_error_0"].replace("%s", d))
-                e = 1
-        if e == 1:
-            print(self.lines['kernel']["startup_error_1"])
-            self.exit()
-        
-        i = list(self.info.values())
-        i.append(self.modules); i.append(None)
-        i.append(self.lines); i.append(self.fs); i.append("[REMOVED FEATURE]"); i.append(self)
-        if not self.boot_opt["safe_mode"]:
-            self.sctl = SystemCTL()
-            self.sctl.start(f"{self.basefs}/../var/events/", i)
-        dcmd = self.json_load("../var/kernel_sets.json")["dynamic_cmd"]
-        if dcmd:
-            self.binaries = load_binaries_dynamic(self.info["shbin"], self.lines['binary'], debug)
-        else:
-            if self.boot_opt["safe_mode"]:
-                self.binaries = load_binaries_dynamic(self.info["shbin"], self.lines['binary'], debug)
-            else:
-                self.binaries = load_binaries(self.info["shbin"], self.lines['binary'], debug)
-        i = list(self.info.values())
-        i.append(self.modules); i.append(self.binaries)
-        i.append(self.lines); i.append(self.fs); i.append("[REMOVED FEATURE]"); i.append(self)
-        if not self.boot_opt["safe_mode"]:
-            for mod in self.modules:
-                self.modules[mod].info["on_load"](i)
-        if not self.boot_opt["safe_mode"]:
-            if "plm" in self.modules:
-                if self.modules["plm"].info["login-manager"]:
-                    self.user = self.modules["plm"].run(i)
-        if "console" in self.modules[self.info["shell"]].info.keys():
-            if self.modules[self.info["shell"]].info["console"]:
-                self.modules[self.info["shell"]].run(i)
+
 
     def safe_boot(self):
             if not os.path.isdir("../etc/kbackup"):
@@ -247,7 +139,7 @@ class BaseKernel:
                 f.write("0")
 
 
-
-            
+# Settings 
+__kernel = BaseKernel
         
         
--- kinit.py
+++ kinit.py
@@ -1,4 +1,4 @@
-import json, os, sys, importlib
+import json, os, sys, importlib, traceback
 
 
 def dynamic_import(module): # Function to import modules
@@ -31,7 +31,7 @@ def boot_recovery(file: str = "recovery", kernel = None) -> None: # Recovery sta
     try:
         r = dynamic_import(file)
     except:
-        print("Emergency: No recovery!!!")
+        print("Emergency: No recovery!!! Something is really wrong!")
     else:
         if kernel == None:
             r.run()
@@ -59,27 +59,53 @@ def boot_recovery(file: str = "recovery", kernel = None) -> None: # Recovery sta
     sys.exit()
     
 def boot_os(fallback: bool = False) -> None: # If fallback - start fallback hooks to start Recovery with base kernel in recovery mode.
+    os.environ["__PYT_OS_ROOT"] = os.getcwd() # Set OS Root to this root.
+
+    scripts = ["etc" + os.path.sep + "init_scripts" + os.path.sep + "normal" + os.path.sep + "init.py", "etc" + os.path.sep + "init_scripts" + os.path.sep + "fallback" + os.path.sep + "init.py"]
+    
     try:
-        kernel = dynamic_import("kernel.kernel")
-    except:
-        print("Emergency: No Kernel found!")
+        kernel = dynamic_import("kernel.kernel") # Trying to import kernel
+    except: 
+        print("Emergency: No Kernel found!") # Boot Recovery without kernel
         boot_recovery()
         
-    with open("./var/build_prop.json", "r") as f:
-        data = json.load(f)
+    with open("./var/build_prop.json", "r") as f: # Loading build data
+        data = json.load(f) 
 
-    if os.path.exists("etc" + os.path.sep + "init_scripts" + os.path.sep + "normal" + os.path.sep + "init.py"): # etc\init_scripts\normal\init.py
-        if not fallback:
-            init = dynamic_import("etc.init_scripts.normal.init")
-            print("Using normal init...")
-    elif os.path.exists("etc" + os.path.sep + "init_scripts" + os.path.sep + "fallback" + os.path.sep + "init.py"): # etc\init_scripts\normal\init.py
+    # If there is normal scripts and we don`t want to use fallback
+    if os.path.exists(scripts[0]) and not fallback: # etc\init_scripts\normal\init.py
+        init = dynamic_import("etc.init_scripts.normal.init")
+        print("Using normal init...")
+
+    # Fallback
+    elif os.path.exists(scripts[1]): # etc\init_scripts\normal\init.py
         init = dynamic_import("etc.init_scripts.fallback.init")
         print("Emergency: Using fallback init...")
-    else:
+        
+    else: # If no scripts, start recovery
         print("Emergency: No init scripts?! Trying to boot Recovery with stock (?) kernel")
         boot_recovery(kernel = kernel.BaseKernel)
 
-    init.init(kernel.BaseKernel, data)
+    try:
+        os.environ["__PYT_KERNEL_ROOT"] = os.path.join(os.getcwd() + os.path.sep + "kernel") # Set Kernel root.
+        init.init(dynamic_import, "kernel.kernel", data) # start Init, with - dynamic_import function, kernel package and data.
+
+    except SystemExit: # if exit - exit ?)
+        sys.exit()
+    
+    except:
+        traceback.print_exc() # If error, print it.
+
+        os.chdir(os.environ["__PYT_OS_ROOT"]) # Change dir to OS Root
+        
+        if not fallback:# If no fallback was used, start it using fallback
+            print("Emergency: Error while using normal startup, use fallback scripts.")
+            boot_os(fallback=True)
+            
+        else: # If Fallback was used, start recovery mode
+            print("Emergency: Starting recovery!")
+            boot_recovery(kernel = kernel.BaseKernel)
+
 
 if __name__ == "__main__":
     main()
